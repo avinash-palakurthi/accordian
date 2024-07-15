ff47c0d1d03d90e871a44aafd4a3fcdb39561901
Avinash <palakurthiavinash07@gmail.com>
2024-07-15 12:12:59 +0530

    data created

---
+++ src/components/data.js
@@ -0,0 +1,29 @@
+const data = [
+	{
+		id: 1,
+		question: " Which planet is known as the “Red Planet ?",
+		answer: "Mars",
+	},
+	{
+		id: 2,
+		question: " What is the tallest mountain in the world?",
+		answer: "Mount Everest",
+	},
+	{
+		id: 3,
+		question: " Which is the largest ocean in the world?",
+		answer: "Pacific Ocean",
+	},
+	{
+		id: 4,
+		question: " What is the largest mammal in the world?",
+		answer: "Blue Whale",
+	},
+	{
+		id: 5,
+		question: " Which country is known as the Land of the Rising Sun?",
+		answer: " Japan",
+	},
+];
+
+export default data;
